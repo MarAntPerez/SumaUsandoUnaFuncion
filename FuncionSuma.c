3a3a85e6eff240f489d2a131d885ac74e74c6d89
Marco <maphy1707@gmail.com>
2023-07-24 22:23:12 -0600

    Suma con una funcion

---
+++ FuncionSuma.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int suma(){
+	
+	int num1 = 7;
+	int num2 = 5;
+	int suma = num1 + num2;
+	return;
+	
+}
+int main(int argc, char *argv[]) {
+	
+	printf("El resultado es: %i", suma());
+	
+	return 0;
+}
